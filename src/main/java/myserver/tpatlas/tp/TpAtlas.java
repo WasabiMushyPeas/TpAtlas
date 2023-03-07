cbc72f0265ee81349f11bdb35c9232f221edf426
WasabiMushyPeas <jackson.mcdowell@gmail.com>
2023-03-06 17:45:28 -0700

    Black project

--- src/main/java/myserver/tpatlas/tp/TpAtlas.java
+++ src/main/java/myserver/tpatlas/tp/TpAtlas.java
@@ -7,6 +7,7 @@ public final class TpAtlas extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("TpAtlas is enabled!");
 
     }
 
